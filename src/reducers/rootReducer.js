f3ea8924f2044a1b4c3a2b9cfbad687a680cbfdf
tbanish <tombanish@gmail.com>
2021-04-16 17:24:29 -0400

    add readingSessions to rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import { books } from './books.js'
+import { readingSessions } from './readingSessions.js'
 
 export const rootReducer = combineReducers({
-  books
+  books,
+  readingSessions
 })
