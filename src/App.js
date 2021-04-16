155d9b25e3fa5d1886ff024173ec4e40601a2d3d
tbanish <tombanish@gmail.com>
2021-04-16 19:07:31 -0400

    render ReadingSessionContainer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { loadBooks } from './actions/books.js'
 import BooksContainer from './containers/BooksContainer.js'
+import ReadingSessionContainer from './containers/ReadingSessionContainer.js'
 import NavBar from './components/NavBar'
 
 class App extends React.Component {
@@ -14,6 +15,7 @@ class App extends React.Component {
       <div>
         <NavBar />
         <BooksContainer />
+        <ReadingSessionContainer />
       </div>
     )
   }
