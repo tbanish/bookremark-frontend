c52862eb78d1fa714cf8e380cff610e2c6574cf6
tbanish <tombanish@gmail.com>
2021-04-16 19:31:32 -0400

    render link to reading sessions from Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Book = (props) => {
+  const renderLinks = () => {
+    return (
+      <Link to={`/books/${props.book.id}/reading-sessions`}>Reading Sessions</Link>
+    )
+  }
+
   return(
     <div>
       <h1>{props.book && props.book.attributes.title}</h1>
       <p>By {props.book && props.book.attributes.author}</p>
+      {renderLinks()}
     </div>
   )
 }
