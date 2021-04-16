9b8d28bc1583506e7ad752df95b984dbc6001143
tbanish <tombanish@gmail.com>
2021-04-16 18:04:41 -0400

    create BookShelf component that renders books where finsihed === true

---
+++ src/components/BookShelf.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const BookShelf = (props) => {
+  const renderBooks = () => {
+    const books = props.books.filter(book => book.attributes.finished === true)
+    return books.map(book => <li key={book.id}><Link to={`/books/${book.id}`}>{book.attributes.title}</Link></li>)
+  }
+
+  return(
+    <div>
+      {renderBooks()}
+    </div>
+  )
+}
+
+export default BookShelf
