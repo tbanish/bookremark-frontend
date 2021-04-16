be7c2f1ea36f5cf3f591d4d2785eb925e8a96123
tbanish <tombanish@gmail.com>
2021-04-16 19:25:40 -0400

    create route for ReadingSession, render component, put routes in Switch

--- src/containers/ReadingSessionContainer.js
+++ src/containers/ReadingSessionContainer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReadingSessionList from '../components/ReadingSessionList'
+import ReadingSession from '../components/ReadingSession'
 import { Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
 
@@ -7,7 +8,10 @@ class ReadingSessionContainer extends React.Component {
   render() {
     return(
       <div>
-        <Route exact path={`/books/:id/reading-sessions`} render={routerProps => <ReadingSessionList {...routerProps} readingSessions={this.props.readingSessions.filter(readingSession => readingSession.id === parseInt(routerProps.match.params.id))}/>}/>
+        <Switch>
+          <Route exact path={`/books/:id/reading-sessions`} render={routerProps => <ReadingSessionList {...routerProps} readingSessions={this.props.readingSessions.filter(readingSession => readingSession.id === parseInt(routerProps.match.params.id))}/>}/>
+          <Route exact path={`/books/:id/reading-sessions/:id`} render={routerProps => <ReadingSession {...routerProps} readingSession={this.props.readingSessions.find(readingSession => readingSession.id === parseInt(routerProps.match.params.id))}/>}/>
+        </Switch>
       </div>
     )
   }
