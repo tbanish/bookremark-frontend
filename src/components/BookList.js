2e8532901617c2cd1ad44df8b55f5a0faef468f2
tbanish <tombanish@gmail.com>
2021-04-16 18:01:50 -0400

    create renderBooks and render list of links of books where finished != true

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const BookList = (props) => {
+  const renderBooks = () => {
+    const books = props.books.filter(book => book.attributes.finished !== true)
+    return books.map(book => <li key={book.id}><Link to={`/books/${book.id}`}>{book.attributes.title}</Link></li>)
+  }
+  
   return(
-    <div>BookList</div>
+    <div>
+      {renderBooks()}
+    </div>
   )
 }
 
