c1af814dc9a83cb65a3bcd08e491e69a3e48de01
tbanish <tombanish@gmail.com>
2021-04-16 18:14:44 -0400

    import Switch and create route /books/:id and pass book down as props to Book component

--- src/containers/BooksContainer.js
+++ src/containers/BooksContainer.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import BookList from '../components/BookList'
 import BookShelf from '../components/BookShelf'
-import { Route } from 'react-router-dom'
+import Book from '../components/Book'
+import { Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 class BooksContainer extends React.Component {
   render() {
     return(
       <div>
-        <Route path="/books/reading-list" render={routerProps => <BookList {...routerProps} books={this.props.books}/>}/>
-        <Route path="/books/bookshelf" render={routerProps => <BookShelf {...routerProps} books={this.props.books}/>}/>
+        <Switch>
+          <Route path="/books/reading-list" render={routerProps => <BookList {...routerProps} books={this.props.books}/>}/>
+          <Route path="/books/bookshelf" render={routerProps => <BookShelf {...routerProps} books={this.props.books}/>}/>
+          <Route path="/books/:id" render={routerProps => <Book {...routerProps} book={this.props.books.find(book => book.id === routerProps.match.params.id)}/>}/>
+        </Switch>
       </div>
     )
   }
