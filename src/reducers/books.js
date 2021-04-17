405333eccf436dee186c18ecaea01e72cb402a68
tbanish <tombanish@gmail.com>
2021-04-17 15:05:54 -0400

    add case ADD_BOOK to books reducer to update state

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -2,6 +2,8 @@ export const books = (state = [], action) => {
   switch (action.type) {
     case 'LOAD_BOOKS':
       return action.payload
+    case 'ADD_BOOK':
+      return state.concat(action.payload)
     default:
       return state
   }
