1af7106443d266b9dabd56f5b3044243da636092
tbanish <tombanish@gmail.com>
2021-04-17 15:05:17 -0400

    build addBook action creator, send POST request to backend to persist new book, dispatch ADD_BOOK to reducer

--- src/actions/books.js
+++ src/actions/books.js
@@ -1,10 +1,32 @@
+const booksEndpoint = 'http://localhost:3001/books'
+
 export const loadBooks = () => {
   return dispatch => {
-    fetch('http://localhost:3001/books')
+    fetch(booksEndpoint)
       .then(resp => resp.json())
       .then(books => {
         dispatch({type: 'LOAD_BOOKS', payload: books.data})
         dispatch({type: 'LOAD_READING_SESSIONS', payload: books.data})
+      })
+  }
+}
+
+export const addBook = (book) => {
+  const body = {
+    title: book.title,
+    author: book.author
+  }
+  return dispatch => {
+    fetch(booksEndpoint, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    })
+      .then(resp => resp.json())
+      .then(book => {
+        dispatch({type: 'ADD_BOOK', payload: book.data})
       })
   }
 }
