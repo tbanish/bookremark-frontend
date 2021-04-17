da96aeece0ebf3688d3536a35696978ac77f829b
tbanish <tombanish@gmail.com>
2021-04-17 16:46:45 -0400

    add case ADD_READING_SESSION, create new reading session and update state

--- src/reducers/readingSessions.js
+++ src/reducers/readingSessions.js
@@ -6,6 +6,15 @@ export const readingSessions = (state = [], action) => {
         readingSessions.push(element.attributes.reading_sessions)
       }
       return readingSessions.flat()
+    case 'ADD_READING_SESSION':
+      const newReadingSession = {
+        id: parseInt(action.payload.id),
+        title: action.payload.attributes.title,
+        duration: action.payload.attributes.duration,
+        date: action.payload.attributes.date,
+        book_id: action.payload.attributes.book.id,
+      }
+      return state.concat(newReadingSession)
     default:
       return state
   }
