9b81c8acb0b03de318014aadab2ce450819e16ef
tbanish <tombanish@gmail.com>
2021-04-17 16:55:16 -0400

    add link to new reading session form

--- src/components/ReadingSession.js
+++ src/components/ReadingSession.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ReadingSession = (props) => {
+  const renderLinks = () => {
+    return(
+      <Link to={`/books/${props.match.params.id}/reading-sessions/new`}>New Reading Session</Link>
+    )
+  }
+
   return(
     <div>
       <h1>{props.readingSession && props.readingSession.title}</h1>
+      {renderLinks()}<br/><br/>
       <p>Date: {props.readingSession && props.readingSession.date}</p>
       <p>Duration: {props.readingSession && props.readingSession.duration}</p>
     </div>
--- src/components/ReadingSessionList.js
+++ src/components/ReadingSessionList.js
@@ -6,9 +6,16 @@ const ReadingSessionList = (props) => {
     return props.readingSessions.map(readingSession => <li key={readingSession.id}><Link to={`/books/${readingSession.book_id}/reading-sessions/${readingSession.id}`}>{readingSession.title}</Link></li>)
   }
 
+  const renderLinks = () => {
+    return(
+      <Link to={`/books/${props.match.params.id}/reading-sessions/new`}>New Reading Session</Link>
+    )
+  }
+
   return(
     <div>
       <h1>Reading Sessions</h1>
+      {renderLinks()}<br/><br/>
       {renderReadingSessions()}
     </div>
   )
