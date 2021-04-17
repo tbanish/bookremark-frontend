af4f4ce0c62ca87c8e07b9f53122dfc9ef57ea34
tbanish <tombanish@gmail.com>
2021-04-17 15:31:53 -0400

    create route for new reading session form and render ReadingSessionNewForm component

--- src/containers/ReadingSessionContainer.js
+++ src/containers/ReadingSessionContainer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReadingSessionList from '../components/ReadingSessionList'
 import ReadingSession from '../components/ReadingSession'
+import ReadingSessionNewForm from '../components/ReadingSessionNewForm'
 import { Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
 
@@ -10,6 +11,7 @@ class ReadingSessionContainer extends React.Component {
       <div>
         <Switch>
           <Route exact path={`/books/:id/reading-sessions`} render={routerProps => <ReadingSessionList {...routerProps} readingSessions={this.props.readingSessions.filter(readingSession => readingSession.id === parseInt(routerProps.match.params.id))}/>}/>
+          <Route exact path={`/books/:id/reading-sessions/new`} render={routerProps => <ReadingSessionNewForm {...routerProps} />}/>
           <Route exact path={`/books/:id/reading-sessions/:id`} render={routerProps => <ReadingSession {...routerProps} readingSession={this.props.readingSessions.find(readingSession => readingSession.id === parseInt(routerProps.match.params.id))}/>}/>
         </Switch>
       </div>
