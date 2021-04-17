382fa1d40148a5e2ecee6fe894c204aaf6ede754
tbanish <tombanish@gmail.com>
2021-04-17 15:36:50 -0400

    add local state to new reading session form, add input for title, submit button, build handleChange function to set state to input value

--- src/components/ReadingSessionNewForm.js
+++ src/components/ReadingSessionNewForm.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 class ReadingSessionNewForm extends React.Component {
+  state = {
+    title: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render() {
     return(
-      <div>ReadingSessionNewForm</div>
+      <div>
+        <form>
+          <input onChange={this.handleChange} type="text" name="title" value={this.state.title} placeholder="title"/><br/><br/>
+          <input type="submit" value="end session"/>
+        </form>
+      </div>
     )
   }
 }
