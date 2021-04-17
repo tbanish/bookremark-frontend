3ae68a6e3cd6b6c9a951085bff201dd47c7aa4cb
tbanish <tombanish@gmail.com>
2021-04-17 15:46:51 -0400

    add date reducer to rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { books } from './books.js'
 import { readingSessions } from './readingSessions.js'
+import { date } from './date.js'
 
 export const rootReducer = combineReducers({
   books,
-  readingSessions
+  readingSessions,
+  date
 })
