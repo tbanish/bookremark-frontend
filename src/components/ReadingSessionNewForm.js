12c92bc378ed19f448c3272ca9a0b21678fdb28d
tbanish <tombanish@gmail.com>
2021-04-17 15:28:19 -0400

    build ReadingSessionNewForm class component

---
+++ src/components/ReadingSessionNewForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class ReadingSessionNewForm extends React.Component {
+  render() {
+    return(
+      <div>ReadingSessionNewForm</div>
+    )
+  }
+}
+
+export default ReadingSessionNewForm
