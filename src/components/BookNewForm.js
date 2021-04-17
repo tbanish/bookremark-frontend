df8ae56a9f841b3754a944f72edc3a20c15dbf61
tbanish <tombanish@gmail.com>
2021-04-17 14:36:24 -0400

    build new book form in BookNewForm component

---
+++ src/components/BookNewForm.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+class BookNewForm extends React.Component {
+  state = {
+    title: '',
+    author: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = () => {
+    
+  }
+
+  render() {
+    return(
+      <div className="BookNewForm">
+        <h1>Add a Book</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange}type="text" name="title" value={this.state.title} placeholder="title"/><br/><br/>
+          <input onChange={this.handleChange}type="text" name="author" value={this.state.author} placeholder="author"/><br/><br/>
+          <input type="submit" value="add book" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default BookNewForm
