c6410e70e7816669798e62d06413f16eb1394813
tbanish <tombanish@gmail.com>
2021-04-18 19:32:20 -0400

    add nested note form in ReadingSessionNewForm component

--- src/components/ReadingSessionNewForm.js
+++ src/components/ReadingSessionNewForm.js
@@ -54,6 +54,11 @@ class ReadingSessionNewForm extends React.Component {
           <Timer setDuration={this.setDuration}/><br/>
           <input type="submit" value="end session"/>
         </form>
+        <form><br/><br/>
+          <input type="text" name="noteTitle" value={this.state.noteTitle} placeholder="give your note a title"/><br/><br/>
+          <textarea type="text" name="noteContent" rows={15} cols={60} value={this.state.noteContent} placeholder="add some notes here ..."/><br/><br/>
+          <input type="submit" value="add note"/>
+        </form>
       </div>
     )
   }
