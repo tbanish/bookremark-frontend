4d590cd1810b0ff9d46e71ff25f1907e1172c5dc
tbanish <tombanish@gmail.com>
2021-04-18 13:20:40 -0400

    connect to store, map state.date to props, set initial local state with title duration and date of reading session

--- src/components/ReadingSessionEditForm.js
+++ src/components/ReadingSessionEditForm.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 class ReadingSessionEditForm extends React.Component {
+  state = {
+    title: this.props.readingSession ? this.props.readingSession.title : '',
+    date: this.props.date,
+    duration: this.props.readingSession ? this.props.readingSession.duration : ''
+  }
+
   render() {
     return(
-      <div>RadingSessionEditForm</div>
+      <div></div>
     )
   }
 }
 
-export default ReadingSessionEditForm
+const mapStateToProps = state => {
+  return {
+    date: state.date
+  }
+}
+
+export default connect(mapStateToProps)(ReadingSessionEditForm)
