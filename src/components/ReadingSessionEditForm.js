14cd895258a131e515aacc8d4d6bcc46477ce969
tbanish <tombanish@gmail.com>
2021-04-18 13:14:29 -0400

    create ReadingSessionEditForm class comonent

---
+++ src/components/ReadingSessionEditForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class ReadingSessionEditForm extends React.Component {
+  render() {
+    return(
+      <div>RadingSessionEditForm</div>
+    )
+  }
+}
+
+export default ReadingSessionEditForm
