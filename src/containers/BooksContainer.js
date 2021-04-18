07438e507c6313bd07af16b4e9abb3e8b458250c
tbanish <tombanish@gmail.com>
2021-04-18 15:25:32 -0400

    map deleteReadingSessions to props and pass down to Book component

--- src/containers/BooksContainer.js
+++ src/containers/BooksContainer.js
@@ -7,6 +7,7 @@ import BookEditForm from '../components/BookEditForm'
 import { Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { addBook, updateBook, deleteBook } from '../actions/books.js'
+import { deleteReadingSessions } from '../actions/readingSessions.js'
 
 
 class BooksContainer extends React.Component {
@@ -17,7 +18,7 @@ class BooksContainer extends React.Component {
           <Route exact path="/books/reading-list" render={routerProps => <BookList {...routerProps} books={this.props.books}/>}/>
           <Route exact path="/books/bookshelf" render={routerProps => <BookShelf {...routerProps} books={this.props.books}/>}/>
           <Route exact path="/books/new" render={routerProps => <BookNewForm {...routerProps} addBook={this.props.addBook}/>}/>
-          <Route exact path="/books/:id" render={routerProps => <Book {...routerProps} deleteBook={this.props.deleteBook} book={this.props.books.find(book => book.id === routerProps.match.params.id)}/>}/>
+          <Route exact path="/books/:id" render={routerProps => <Book {...routerProps} deleteBook={this.props.deleteBook} deleteReadingSessions={this.props.deleteReadingSessions} book={this.props.books.find(book => book.id === routerProps.match.params.id)}/>}/>
           <Route exact path="/books/:id/edit" render={routerProps => <BookEditForm {...routerProps} updateBook={this.props.updateBook} book={this.props.books.find(book => book.id === routerProps.match.params.id)}/>}/>
         </Switch>
       </div>
@@ -31,4 +32,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, { addBook, updateBook, deleteBook })(BooksContainer)
+export default connect(mapStateToProps, { addBook, updateBook, deleteBook, deleteReadingSessions })(BooksContainer)
