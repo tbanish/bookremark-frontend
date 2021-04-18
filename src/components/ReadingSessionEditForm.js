641e49488f0662ae198f4f1c233df1bbeae45ab2
tbanish <tombanish@gmail.com>
2021-04-18 14:01:35 -0400

    build reading session edit form, handleChange to update local state, invoke updateReadingSession to update the reading session in backend and store

--- src/components/ReadingSessionEditForm.js
+++ src/components/ReadingSessionEditForm.js
@@ -8,9 +8,34 @@ class ReadingSessionEditForm extends React.Component {
     duration: this.props.readingSession ? this.props.readingSession.duration : ''
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const readingSession = {
+      title: this.state.title,
+      date: this.state.date,
+      duration: this.state.duration,
+      book_id: this.props.readingSession.book_id,
+    }
+
+    this.props.updateReadingSession(readingSession, this.props.readingSession.id)
+  }
+
   render() {
     return(
-      <div></div>
+      <div>
+        <p>Duration: {this.state.duration}</p>
+        <p>Date: {this.state.date}</p>
+        <form onSubmit={this.handleSubmit}>
+          Title: <input onChange={this.handleChange} type="text" name="title" value={this.state.title}/><br/><br/>
+          <input type="submit" value="update reading session" />
+        </form>
+      </div>
     )
   }
 }
