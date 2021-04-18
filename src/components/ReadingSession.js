728e9c9e5aad0eae41a5dcfdc9ccb5663b9c308f
tbanish <tombanish@gmail.com>
2021-04-18 19:05:34 -0400

    create renderNotes function, render notes associated with reading session

--- src/components/ReadingSession.js
+++ src/components/ReadingSession.js
@@ -6,7 +6,7 @@ const ReadingSession = (props) => {
     return(
       <div>
         <Link to={`/books/${props.match.params.id}/reading-sessions/new`}>New Reading Session</Link><br/>
-        <Link to={`/books/${props.readingSession.book_id}/reading-sessions/${props.readingSession.id}/edit`}>Edit Reading Session</Link>
+        <Link to={`/books/${props.readingSession && props.readingSession.book_id}/reading-sessions/${props.readingSession && props.readingSession.id}/edit`}>Edit Reading Session</Link>
       </div>
     )
   }
@@ -16,13 +16,23 @@ const ReadingSession = (props) => {
     props.history.push(`/books/${props.readingSession.book_id}/reading-sessions`)
   }
 
+  const renderNotes = () => {
+    return props.notes.map(note =>
+      <div>
+        <h2>{note.title}</h2>
+        <p>{note.content}</p>
+      </div>
+    )
+  }
+
   return(
     <div>
       <h1>{props.readingSession && props.readingSession.title}</h1>
       {renderLinks()}<br/><br/>
       <p>Date: {props.readingSession && props.readingSession.date}</p>
       <p>Duration: {props.readingSession && props.readingSession.duration}</p>
-      <input onClick={() => handleClick()} type="button" value="delete"/>
+      <input onClick={() => handleClick()} type="button" value="delete"/><br/>
+      {renderNotes()}
     </div>
   )
 }
