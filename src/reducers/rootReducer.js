2d411b4466d74553eda3594cc25514a01ebb069f
tbanish <tombanish@gmail.com>
2021-04-18 18:22:20 -0400

    add notes to rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import { books } from './books.js'
 import { readingSessions } from './readingSessions.js'
+import { notes } from './notes.js'
 import { date } from './date.js'
 
 export const rootReducer = combineReducers({
   books,
   readingSessions,
+  notes,
   date
 })
