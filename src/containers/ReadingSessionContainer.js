2b0610fa5aa72fbf97241aabb5db7e9982484842
tbanish <tombanish@gmail.com>
2021-04-18 14:07:20 -0400

    import addReadingSession action, map dispatch to props, pass down to ReadingSessionNewForm, map state.date to props, pass down to ReadingSessionNewForm

--- src/containers/ReadingSessionContainer.js
+++ src/containers/ReadingSessionContainer.js
@@ -5,7 +5,7 @@ import ReadingSessionNewForm from '../components/ReadingSessionNewForm'
 import ReadingSessionEditForm from '../components/ReadingSessionEditForm'
 import { Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { updateReadingSession } from '../actions/readingSessions'
+import { addReadingSession, updateReadingSession } from '../actions/readingSessions'
 
 class ReadingSessionContainer extends React.Component {
   render() {
@@ -13,7 +13,7 @@ class ReadingSessionContainer extends React.Component {
       <div>
         <Switch>
           <Route exact path={`/books/:id/reading-sessions`} render={routerProps => <ReadingSessionList {...routerProps} readingSessions={this.props.readingSessions.filter(readingSession => readingSession.book_id === parseInt(routerProps.match.params.id))}/>}/>
-          <Route exact path={`/books/:id/reading-sessions/new`} render={routerProps => <ReadingSessionNewForm {...routerProps} />}/>
+          <Route exact path={`/books/:id/reading-sessions/new`} render={routerProps => <ReadingSessionNewForm addReadingSession={this.props.addReadingSession} date={this.props.date} {...routerProps} />}/>
           <Route exact path={`/books/:id/reading-sessions/:id`} render={routerProps => <ReadingSession {...routerProps} readingSession={this.props.readingSessions.find(readingSession => readingSession.id === parseInt(routerProps.match.params.id))}/>}/>
           <Route exact path={`/books/:id/reading-sessions/:id/edit`} render={routerProps => <ReadingSessionEditForm {...routerProps} updateReadingSession={this.props.updateReadingSession} readingSession={this.props.readingSessions.find(readingSession => readingSession.id === parseInt(routerProps.match.params.id))}/>}/>
         </Switch>
@@ -24,8 +24,9 @@ class ReadingSessionContainer extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    readingSessions: state.readingSessions
+    readingSessions: state.readingSessions,
+    date: state.date
   }
 }
 
-export default connect(mapStateToProps, { updateReadingSession })(ReadingSessionContainer)
+export default connect(mapStateToProps, { addReadingSession, updateReadingSession })(ReadingSessionContainer)
