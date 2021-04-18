e3325d2b0fbe953e89161205408c5082a1fc140e
tbanish <tombanish@gmail.com>
2021-04-18 11:34:39 -0400

    render book edit form, set initial state to current title and author, create handleChange function to change state on input change

--- src/components/BookEditForm.js
+++ src/components/BookEditForm.js
@@ -1,9 +1,32 @@
 import React from 'react'
 
 class BookEditForm extends React.Component {
+  state = {
+    title: this.props.book ? this.props.book.attributes.title : '',
+    author: this.props.book ? this.props.book.attributes.author : ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+  }
+
   render() {
+
     return(
-      <div>BookEditForm</div>
+      <div className="BookNewForm">
+        <h1>Edit Book</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange}type="text" name="title" value={this.state.title} placeholder="title"/><br/><br/>
+          <input onChange={this.handleChange}type="text" name="author" value={this.state.author} placeholder="author"/><br/><br/>
+          <input type="submit" value="edit book" />
+        </form>
+      </div>
     )
   }
 }
