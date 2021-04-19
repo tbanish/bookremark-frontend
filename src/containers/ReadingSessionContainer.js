643457e00676f56da99736638b4c2d86d49e192e
tbanish <tombanish@gmail.com>
2021-04-19 11:45:09 -0400

    map deleteNotes dispatch to props and pass down to ReadingSession component

--- src/containers/ReadingSessionContainer.js
+++ src/containers/ReadingSessionContainer.js
@@ -6,7 +6,7 @@ import ReadingSessionEditForm from '../components/ReadingSessionEditForm'
 import { Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { addReadingSession, updateReadingSession, deleteReadingSession } from '../actions/readingSessions'
-import { deleteNote } from '../actions/notes.js'
+import { deleteNote, deleteNotes } from '../actions/notes.js'
 
 class ReadingSessionContainer extends React.Component {
   render() {
@@ -15,7 +15,7 @@ class ReadingSessionContainer extends React.Component {
         <Switch>
           <Route exact path={`/books/:id/reading-sessions`} render={routerProps => <ReadingSessionList {...routerProps} readingSessions={this.props.readingSessions.filter(readingSession => readingSession.book_id === parseInt(routerProps.match.params.id))}/>}/>
           <Route exact path={`/books/:id/reading-sessions/new`} render={routerProps => <ReadingSessionNewForm addReadingSession={this.props.addReadingSession} date={this.props.date} {...routerProps} />}/>
-          <Route exact path={`/books/:id/reading-sessions/:id`} render={routerProps => <ReadingSession {...routerProps} deleteNote={this.props.deleteNote} notes={this.props.notes.filter(note => note.readingSessionId === parseInt(routerProps.match.params.id))} deleteReadingSession={this.props.deleteReadingSession} readingSession={this.props.readingSessions.find(readingSession => readingSession.id === parseInt(routerProps.match.params.id))}/>}/>
+          <Route exact path={`/books/:id/reading-sessions/:id`} render={routerProps => <ReadingSession {...routerProps} deleteNote={this.props.deleteNote} deleteNotes={this.props.deleteNotes} notes={this.props.notes.filter(note => note.readingSessionId === parseInt(routerProps.match.params.id))} deleteReadingSession={this.props.deleteReadingSession} readingSession={this.props.readingSessions.find(readingSession => readingSession.id === parseInt(routerProps.match.params.id))}/>}/>
           <Route exact path={`/books/:id/reading-sessions/:id/edit`} render={routerProps => <ReadingSessionEditForm {...routerProps} updateReadingSession={this.props.updateReadingSession} readingSession={this.props.readingSessions.find(readingSession => readingSession.id === parseInt(routerProps.match.params.id))}/>}/>
         </Switch>
       </div>
@@ -31,4 +31,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, { addReadingSession, updateReadingSession, deleteReadingSession, deleteNote })(ReadingSessionContainer)
+export default connect(mapStateToProps, { addReadingSession, updateReadingSession, deleteReadingSession, deleteNote, deleteNotes })(ReadingSessionContainer)
