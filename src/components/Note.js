c8cd794bb7069c714c79a8bf3da74bd7390066b2
tbanish <tombanish@gmail.com>
2021-04-19 09:23:30 -0400

    create link for note edit form and render link

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Note = (props) => {
+  const renderLinks = () => {
+    return <Link to={`/books/${props.note && props.note.bookId}/notes/${props.note && props.note.id}/edit`}>Edit</Link>
+  }
+
   return(
     <div>
       <h1>{props.note && props.note.title}</h1>
       <p>{props.note && props.note.content}</p>
+      {renderLinks()}
     </div>
   )
 }
