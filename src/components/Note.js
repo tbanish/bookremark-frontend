9521c86cd3e812f645567de5cdd4a1776f8f6108
tbanish <tombanish@gmail.com>
2021-04-19 09:42:55 -0400

    build delete button in Note Component and invoke deleteNote to delete note

--- src/components/Note.js
+++ src/components/Note.js
@@ -6,10 +6,16 @@ const Note = (props) => {
     return <Link to={`/books/${props.note && props.note.bookId}/notes/${props.note && props.note.id}/edit`}>Edit</Link>
   }
 
+  const handleClick = () => {
+    props.deleteNote(props.note.id)
+    props.history.push(`/books/${props.note.bookId}`)
+  }
+
   return(
     <div>
       <h1>{props.note && props.note.title}</h1>
-      <p>{props.note && props.note.content}</p>
+      <p>{props.note && props.note.content}</p><br/>
+      <input onClick={() => handleClick()} type="button" value="delete"/><br/><br/>
       {renderLinks()}
     </div>
   )
