6009f9ef400ce5a362329a626ea8e35b179ed0a8
tbanish <tombanish@gmail.com>
2021-04-20 09:47:47 -0400

    add case ADD_GOAL to reducer to add goal to state

--- src/reducers/goals.js
+++ src/reducers/goals.js
@@ -2,6 +2,8 @@ export const goals = (state = [], action) => {
   switch (action.type) {
     case 'LOAD_GOALS':
       return action.payload
+    case 'ADD_GOAL':
+      return [action.payload]
     default:
       return state
   }
