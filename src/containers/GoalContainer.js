5ce40a29cd4231cee6caecb70bbe677558422a6c
tbanish <tombanish@gmail.com>
2021-04-20 10:21:34 -0400

    map updateGoal dispatch to props and pass down to GoalNewForm

--- src/containers/GoalContainer.js
+++ src/containers/GoalContainer.js
@@ -3,7 +3,7 @@ import Goal from '../components/Goal'
 import GoalNewForm from '../components/GoalNewForm'
 import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { addGoal } from '../actions/goals.js'
+import { addGoal, updateGoal } from '../actions/goals.js'
 
 class GoalContainer extends React.Component {
   render() {
@@ -11,7 +11,7 @@ class GoalContainer extends React.Component {
       <div>
         <Route exact path={'/'} render={routerProps => <Goal {...routerProps} books={this.props.books.filter(book => book.attributes.finished === true)} goals={this.props.goals}/>}/>
         <Route exact path={'/goals/new'} render={routerProps => <GoalNewForm {...routerProps} addGoal={this.props.addGoal} goals={this.props.goals} />}/>
-        <Route exact path={'/goals/:id/edit'} render={routerProps => <GoalNewForm goals={this.props.goals} />}/>
+        <Route exact path={'/goals/:id/edit'} render={routerProps => <GoalNewForm {...routerProps} updateGoal={this.props.updateGoal} goals={this.props.goals} />}/>
       </div>
     )
   }
@@ -24,4 +24,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, { addGoal })(GoalContainer)
+export default connect(mapStateToProps, { addGoal, updateGoal })(GoalContainer)
