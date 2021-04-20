1ba3cf7729976d005678b0526ce01ae4809d38fe
tbanish <tombanish@gmail.com>
2021-04-20 10:20:35 -0400

    add case UPDATE_GOAL to reducer to update state

--- src/reducers/goals.js
+++ src/reducers/goals.js
@@ -4,6 +4,8 @@ export const goals = (state = [], action) => {
       return action.payload
     case 'ADD_GOAL':
       return [action.payload]
+    case 'UPDATE_GOAL':
+      return [action.payload]
     default:
       return state
   }
