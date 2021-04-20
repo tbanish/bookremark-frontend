78109f61b329c3dff68bb0598f970f996585e213
tbanish <tombanish@gmail.com>
2021-04-19 20:07:33 -0400

    add goals reducer to rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,10 +3,12 @@ import { books } from './books.js'
 import { readingSessions } from './readingSessions.js'
 import { notes } from './notes.js'
 import { date } from './date.js'
+import { goals } from './goals.js'
 
 export const rootReducer = combineReducers({
   books,
   readingSessions,
   notes,
-  date
+  date,
+  goals
 })
