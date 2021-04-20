c5ecdaf5fcde23a4af227e77f823aeb4203b9b73
tbanish <tombanish@gmail.com>
2021-04-20 12:05:31 -0400

    refactor renderGoalContent into separate functions, set condition to display if a user has reached there goal or not

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -11,21 +11,65 @@ const Goal = (props) => {
     props.deleteGoal(id)
   }
 
+  const renderYearlyGoal = () => {
+    const goal = props.goals[0]
+    return (
+      <>
+        <h2>Yearly Reading Goal</h2>
+        <p>{goal.attributes.total} books/year</p>
+      </>
+    )
+  }
+
+  const renderBooksRemaining = () => {
+    const goal = props.goals[0]
+    const remaining = goal.attributes.remaining
+    if (remaining > 0) {
+      return (
+        <>
+          <p>Read {remaining} more book(s) to reach your goal for the year.</p>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <p>You have reached your goal for the year!</p>
+        </>
+      )
+    }
+
+  }
+
+  const renderDeleteandEdit = () => {
+    const goal = props.goals[0]
+    return(
+      <>
+        <input onClick={() => handleClick()}type="button" value="delete goal"/><br/><br/>
+        <Link to={`/goals/${goal.id}/edit`}>Edit Goal</Link>
+      </>
+    )
+  }
+
+  const renderBooksReadThisYear = () => {
+    const goal = props.goals[0]
+    return (
+      <>
+        <h3>Books Read This Year</h3>
+        <p>You have read {goal.attributes.total - goal.attributes.remaining} book(s) this year.</p>
+        {renderFinishedBooks()}
+      </>
+    )
+  }
+
   const renderGoalContent = () => {
     if (props.goals.length > 0) {
       const goal = props.goals[0]
       return (
         <div>
-          <h2>Yearly Reading Goal</h2>
-          <p>{goal.attributes.total} books/year</p>
-          <p>Read {goal.attributes.remaining} more books to reach your goal for the year.</p>
-          <input onClick={() => handleClick()}type="button" value="delete goal"/><br/><br/>
-          <Link to={`/goals/${goal.id}/edit`}>Edit Goal</Link>
-
-
-          <h3>Books Read This Year</h3>
-          <p>You have read {goal.attributes.total - goal.attributes.remaining} book(s) this year.</p>
-          {renderFinishedBooks()}
+          {renderYearlyGoal()}
+          {renderBooksRemaining()}
+          {renderDeleteandEdit()}
+          {renderBooksReadThisYear()}
         </div>
       )
       } else {
