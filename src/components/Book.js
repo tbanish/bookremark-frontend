2d8d1ac56ec902a76070a1e63c3abff21a0c31c2
tbanish <tombanish@gmail.com>
2021-04-23 12:34:55 -0400

    add link to new reading session form

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,6 +6,7 @@ const Book = (props) => {
   const renderLinks = () => {
     return (
       <div>
+        <Link to={`/books/${props.book && props.book.id}/reading-sessions/new`}>New Reading Session</Link><br/><br/>
         <Link to={`/books/${props.book && props.book.id}/reading-sessions`}>Reading Sessions</Link><br/><br/>
         <Link to={`/books/${props.book && props.book.id}/edit`}>Edit Book</Link>
       </div>
