b57fd6923176dafa0498dcb9f9f10b1b658388b3
tbanish <tombanish@gmail.com>
2021-04-23 10:22:03 -0400

    renderContent displays message to user if there aren't any books

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,7 +4,16 @@ import { Link } from 'react-router-dom'
 const BookList = (props) => {
   const renderBooks = () => {
     const books = props.books.filter(book => book.attributes.finished !== true)
-    return books.map(book => <li key={book.id}><Link to={`/books/${book.id}`}>{book.attributes.title}</Link></li>)
+    if (books.length > 0) {
+      return books.map(book => <li key={book.id}><Link to={`/books/${book.id}`}>{book.attributes.title}</Link></li>)
+    } else {
+      return (
+        <div>
+          <h2>Nothing to read?</h2>
+          <p>Click <Link to="/books/new">here</Link> to start adding books to your reading list.</p>
+        </div>
+      )
+    }
   }
 
   return(
