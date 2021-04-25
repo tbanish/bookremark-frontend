21cc3f9fb75470e7d77454f578564dbbc1de2177
tbanish <tombanish@gmail.com>
2021-04-25 16:57:29 -0400

    import and add FaBookDead icon

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import '../css/NoMatch.css'
+import { FaBookDead } from "react-icons/fa";
 
 const NoMatch = () => {
   return (
-    <div>
+    <div className="NoMatch">
       <h1>404.</h1>
       <h2>uh oh ... we're not sure what you're looking for.</h2>
       <p>a little lost?</p>
       <p>maybe take a break and <Link to={'/books/reading-list'}>go read a book</Link>.</p>
+      <FaBookDead className="FaBookDead"/>
     </div>
   )
 }
