1882d6e34d63ae6e52483d6e7c7302d680d05862
tbanish <tombanish@gmail.com>
2021-04-25 19:25:52 -0400

    add h3 header for links

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,6 +7,7 @@ const Book = (props) => {
   const renderLinks = () => {
     return (
       <div>
+        <h3>Links</h3>
         <Link to={`/books/${props.book && props.book.id}/reading-sessions/new`}>New Reading Session</Link><br/><br/>
         <Link to={`/books/${props.book && props.book.id}/reading-sessions`}>Reading Sessions</Link><br/><br/>
         <Link to={`/books/${props.book && props.book.id}/edit`}>Edit Book</Link>
