4ca49698a0f3f6350a71f79317776ab84bf413c8
tbanish <tombanish@gmail.com>
2021-04-25 15:06:42 -0400

    give GoalContainer className

--- src/containers/GoalContainer.js
+++ src/containers/GoalContainer.js
@@ -8,7 +8,7 @@ import { addGoal, updateGoal, deleteGoal } from '../actions/goals.js'
 class GoalContainer extends React.Component {
   render() {
     return(
-      <div>
+      <div className="GoalContainer">
         <Route exact path={'/'} render={routerProps => <Goal {...routerProps} deleteGoal={this.props.deleteGoal} books={this.props.books.filter(book => book.attributes.finished === true)} goals={this.props.goals}/>}/>
         <Route exact path={'/goals/new'} render={routerProps => <GoalNewForm {...routerProps} addGoal={this.props.addGoal} goals={this.props.goals} />}/>
         <Route exact path={'/goals/:id/edit'} render={routerProps => <GoalNewForm {...routerProps} updateGoal={this.props.updateGoal} goals={this.props.goals} />}/>
