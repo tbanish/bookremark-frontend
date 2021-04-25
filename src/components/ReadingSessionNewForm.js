c252eed783d4c64afe942f6c382ac7e3b3a25990
tbanish <tombanish@gmail.com>
2021-04-25 15:32:04 -0400

    import ReadingSessionNewForm.css

--- src/components/ReadingSessionNewForm.js
+++ src/components/ReadingSessionNewForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Timer from './Timer.js'
 import uuid from 'uuid'
+import '../css/ReadingSessionNewForm.css'
 
 class ReadingSessionNewForm extends React.Component {
   state = {
