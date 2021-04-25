0fa56328ce5d17238118d4f341968bf874925c27
tbanish <tombanish@gmail.com>
2021-04-25 19:26:24 -0400

    remove pipes between nav links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,9 @@ import '../css/NavBar.css'
 const NavBar = () => {
   return(
     <div className="NavBar">
-      <NavLink to="/">Home | </NavLink>
-      <NavLink to="/books/reading-list">Reading List | </NavLink>
-      <NavLink to="/books/bookshelf">Book Shelf | </NavLink>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/books/reading-list">Reading List</NavLink>
+      <NavLink to="/books/bookshelf">Book Shelf</NavLink>
       <NavLink to="/books/new">Add a Book</NavLink>
     </div>
   )
