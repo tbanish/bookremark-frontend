e6b3490bf7a6e6f853d310710da82fb864f8bf68
tbanish <tombanish@gmail.com>
2021-04-25 16:48:30 -0400

    import and add ImBooks icon component

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../css/BookShelf.css'
+import { ImBooks } from "react-icons/im";
 
 const BookShelf = (props) => {
   const renderBooks = () => {
@@ -11,6 +12,7 @@ const BookShelf = (props) => {
       return (
         <div>
           <p>When you finish a book it will be stored here on your bookshelf where you can always access past notes and reading sessions.</p>
+          <div className="ImBooks"><ImBooks /></div>
         </div>
       )
     }
