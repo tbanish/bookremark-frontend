bef522c718d557a266c8f45f2cf2427ce6439813
tbanish <tombanish@gmail.com>
2021-04-25 21:47:53 -0400

    add className to reading session note and add id to date and duration p tags

--- src/components/ReadingSession.js
+++ src/components/ReadingSession.js
@@ -25,7 +25,7 @@ const ReadingSession = (props) => {
 
   const renderNotes = () => {
     return props.notes.map(note =>
-      <div key={note.id}>
+      <div className="readingSessionNote" key={note.id}>
         <h2>{note.title}</h2>
         <p>{note.content}</p>
         <input onClick={(event) => handleDeleteNote(event)} id={note.id} type="button" value="delete note"/>
@@ -39,9 +39,9 @@ const ReadingSession = (props) => {
         <>
           <div id="readingSessioncol1">
             <h2>{props.readingSession && props.readingSession.title}</h2>
-            {renderLinks()}<br/><br/>
-            <p>Date: {props.readingSession && props.readingSession.date}</p>
-            <p>Duration: {props.readingSession && props.readingSession.duration} minutes</p>
+            {renderLinks()}
+          <p id="readingSessionDate">Date: {props.readingSession && props.readingSession.date}</p>
+            <p id="readingSessionDuration">Duration: {props.readingSession && props.readingSession.duration} minutes</p>
             <input onClick={() => handleClick()} type="button" value="delete"/><br/>
           </div>
           <div id="readingSessioncol2">
