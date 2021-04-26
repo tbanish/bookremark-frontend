e44f4d3861461a2d5338f62d484553ae9b7b0714
tbanish <tombanish@gmail.com>
2021-04-25 22:03:49 -0400

    create Home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return(
+    <div className="Home">
+      <div classNam="hero">
+        <h1>Book Remark</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
