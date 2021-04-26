08638ab6c83991518b726696d80f6b5ab30aff8c
tbanish <tombanish@gmail.com>
2021-04-25 21:44:20 -0400

    import and render GiRead icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import { GiRead } from "react-icons/gi";
 import '../css/NavBar.css'
 
 const NavBar = () => {
   return(
     <div className="NavBar">
+      <GiRead className="GiRead"/><br/>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/books/reading-list">Reading List</NavLink>
       <NavLink to="/books/bookshelf">Book Shelf</NavLink>
