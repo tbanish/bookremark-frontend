d69419806bf30c2960dd20078f1e2e8693019331
tbanish <tombanish@gmail.com>
2021-04-27 13:02:09 -0400

    import colors; divColor() - returns random background color; add className to book link, add style to book link div, add p tag for author; build renderQuote(); put quote and h1 in bookListBanner div and renderBooks in bookListGrid

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,12 +2,25 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../css/BookList.css'
 import { BsBookHalf } from "react-icons/bs";
+import { colors } from '../data.js'
 
 const BookList = (props) => {
+  const divColor = () => {
+    let style = {backgroundColor: ''}
+    const color = colors[Math.floor(Math.random() * colors.length)]
+    style.backgroundColor = color
+    return style
+  }
+
   const renderBooks = () => {
     const books = props.books.filter(book => book.attributes.finished !== true)
     if (books.length > 0) {
-      return books.map(book => <li key={book.id}><Link to={`/books/${book.id}`}>{book.attributes.title}</Link></li>)
+      return books.map(book =>
+        <div key={book.id} style={divColor()} className="bookCard">
+          <Link className="bookLink" to={`/books/${book.id}`}>{book.attributes.title}</Link>
+          <p>{book.attributes.author}</p>
+        </div>
+      )
     } else {
       return (
         <div>
@@ -19,10 +32,23 @@ const BookList = (props) => {
     }
   }
 
+  const renderQuote = () => {
+    return (
+      <p className="quote" id="bookListQuote">
+        “Beware of the person of one book.” – Thomas Aquinas
+      </p>
+    )
+  }
+
   return(
     <div className="BookList">
-      <h1>Reading List</h1>
-      {renderBooks()}
+      <div className="bookListBanner">
+        <h1>Reading List</h1>
+        {renderQuote()}
+      </div>
+      <div className="bookListGrid">
+        {renderBooks()}
+      </div>
     </div>
   )
 }
