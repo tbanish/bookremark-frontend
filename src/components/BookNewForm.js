ed5837670da2f4f62fb72cbfdcabfbc06b049a8c
tbanish <tombanish@gmail.com>
2021-04-27 16:21:53 -0400

    add description and page count to state; build renderOptions(); modify form to accept optional description and page count; add asterisk to required fields;

--- src/components/BookNewForm.js
+++ src/components/BookNewForm.js
@@ -4,7 +4,9 @@ import '../css/BookNewForm.css'
 class BookNewForm extends React.Component {
   state = {
     title: '',
-    author: ''
+    author: '',
+    description: '',
+    pageCount: ''
   }
 
   handleChange = (event) => {
@@ -19,13 +21,26 @@ class BookNewForm extends React.Component {
     this.props.history.push('/books/reading-list')
   }
 
+  renderOptions = () => {
+    let options = []
+    for (let i = 0; i < 1000; i++) {
+      options.push(<option key={i} value={i}>{i}</option>)
+    }
+    return options
+  }
+
   render() {
     return(
       <div className="BookNewForm">
         <h1>Add a Book</h1>
         <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange}type="text" name="title" value={this.state.title} placeholder="title"/><br/><br/>
-          <input onChange={this.handleChange}type="text" name="author" value={this.state.author} placeholder="author"/><br/><br/>
+          <span className="requiredField">*</span><input onChange={this.handleChange}type="text" name="title" value={this.state.title} placeholder="title"/><br/><br/>
+          <span className="requiredField">*</span><input onChange={this.handleChange}type="text" name="author" value={this.state.author} placeholder="author"/><br/><br/>
+          <label>Page Count: </label>
+          <select value={this.state.pageCount} name="pageCount" onChange={this.handleChange}>
+            {this.renderOptions()}
+          </select><br/><br/>
+          <textarea onChange={this.handleChange} type="text" name="description" rows={20} cols={50}value={this.state.description} placeholder="add a book description (optional)"/><br/>
           <input type="submit" value="add book" />
         </form>
       </div>
