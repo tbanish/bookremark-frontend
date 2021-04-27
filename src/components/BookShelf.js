29c004436c5756554dcb6a97efaa71b9b76f0c67
tbanish <tombanish@gmail.com>
2021-04-27 14:52:25 -0400

    import colors, build divColor(), put links in bookShelfCard div, render author p tag, renderQuote(), create div for bookShelfBanner and bookShelfGrid

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -2,12 +2,25 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../css/BookShelf.css'
 import { ImBooks } from "react-icons/im";
+import {colors} from '../data.js'
 
 const BookShelf = (props) => {
+  const divColor = () => {
+    let style = {backgroundColor: ''}
+    const color = colors[Math.floor(Math.random() * colors.length)]
+    style.backgroundColor = color
+    return style
+  }
+
   const renderBooks = () => {
     const books = props.books.filter(book => book.attributes.finished === true)
     if (books.length > 0) {
-      return books.map(book => <li key={book.id}><Link to={`/books/${book.id}`}>{book.attributes.title}</Link></li>)
+      return books.map(book =>
+        <div key={book.id} style={divColor()} className="bookShelfCard">
+          <Link className="bookLink" to={`/books/${book.id}`}>{book.attributes.title}</Link>
+          <p>{book.attributes.author}</p>
+        </div>
+      )
     } else {
       return (
         <div>
@@ -18,10 +31,21 @@ const BookShelf = (props) => {
     }
   }
 
+  const renderQuote = () => {
+    return (
+      <p className="quote" id="bookShelfQuote">“Once you have read a book you care about, some part of it is always with you.” – Louis L’Amour</p>
+    )
+  }
+
   return(
     <div className="BookShelf">
-      <h1>Book Shelf</h1>
-      {renderBooks()}
+      <div className="bookShelfBanner">
+        <h1>Book Shelf</h1>
+        {renderQuote()}
+      </div>
+      <div className="bookShelfGrid">
+        {renderBooks()}
+      </div>
     </div>
   )
 }
