2373f5ee4d8523c02daacaf77ec8b7156dc25f71
tbanish <tombanish@gmail.com>
2021-04-27 10:38:16 -0400

    add goalToal id to books/year, add booksRemaining id to remaining books, add goalReached id to reached goal message, add id deleteGoalbtn, put books read this year in div, build renderQuote, create goalContent columns, add className setGoal

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -17,8 +17,7 @@ const Goal = (props) => {
     const goal = props.goals[0]
     return (
       <>
-        <h2>Yearly Reading Goal</h2>
-        <p>{goal.attributes.total} books/year</p>
+        <p id="goalTotal">{goal.attributes.total} books/year</p>
       </>
     )
   }
@@ -29,13 +28,13 @@ const Goal = (props) => {
     if (remaining > 0) {
       return (
         <>
-          <p>Read {remaining} more book(s) to reach your goal for the year.</p>
+          <p id="booksRemaining" >Read {remaining} more book(s) to reach your goal for the year.</p>
         </>
       )
     } else {
       return (
         <>
-          <p>You have reached your goal for the year!</p>
+          <p id="goalReached">You have reached your goal for the year!</p>
         </>
       )
     }
@@ -46,7 +45,7 @@ const Goal = (props) => {
     const goal = props.goals[0]
     return(
       <>
-        <input onClick={() => handleClick()}type="button" value="delete goal"/><br/><br/>
+        <input id="deleteGoalbtn" onClick={() => handleClick()}type="button" value="Delete Goal"/><br/><br/>
         <Link to={`/goals/${goal.id}/edit`}>Edit Goal</Link>
       </>
     )
@@ -55,27 +54,44 @@ const Goal = (props) => {
   const renderBooksReadThisYear = () => {
     const goal = props.goals[0]
     return (
-      <>
+      <div>
         <h3>Books Read This Year</h3>
         <p>You have read {goal.attributes.total - goal.attributes.remaining} book(s) this year.</p>
         {renderFinishedBooks()}
-      </>
+      </div>
+    )
+  }
+
+  const renderQuote = () => {
+    return (
+      <div className="quote" id="goalQuote">
+      <p>“It is good to have an end to journey toward; but it is the journey that matters, in the end.”</p>
+      <p>― Ursula K. Le Guin, The Left Hand of Darkness</p>
+      </div>
     )
   }
 
   const renderGoalContent = () => {
     if (props.goals.length > 0) {
       return (
-        <div>
-          {renderYearlyGoal()}
-          {renderBooksRemaining()}
-          {renderDeleteandEdit()}
+        <>
+        <div className="goalContentcol" id="goalContentcol1">
           {renderBooksReadThisYear()}
         </div>
+        <div className="goalContentcol" id="goalContentcol2">
+            <h2>Yearly Reading Goal</h2>
+            <div className="col2Body">
+              {renderYearlyGoal()}
+              {renderBooksRemaining()}
+              {renderDeleteandEdit()}
+              {renderQuote()}
+            </div>
+        </div>
+        </>
       )
       } else {
         return (
-          <div>
+          <div className="setGoal">
             <h2>Yearly Reading Goal</h2>
             <p>Start your reading habit by setting your yearly goal.</p>
             <Link to={`/goals/new`}>Set Goal</Link>
