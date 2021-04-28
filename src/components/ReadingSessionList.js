4bf30ddbf3ca03305007ed6739345f9c2340f6ca
tbanish <tombanish@gmail.com>
2021-04-28 19:06:05 -0400

    change reading session link parameter to readingSession.book_id; change reading session header to h3;

--- src/components/ReadingSessionList.js
+++ src/components/ReadingSessionList.js
@@ -4,19 +4,13 @@ import '../css/ReadingSessionList.css'
 
 const ReadingSessionList = (props) => {
   const renderReadingSessions = () => {
-    return props.readingSessions.map(readingSession => <li key={readingSession.id}><Link to={`/books/${readingSession.book_id}/reading-sessions/${readingSession.id}`}>{readingSession.title}</Link></li>)
-  }
-
-  const renderLinks = () => {
-    return(
-      <Link to={`/books/${props.match.params.id}/reading-sessions/new`}>New Reading Session</Link>
-    )
+    return props.readingSessions.map(readingSession =>
+      <li key={readingSession.id}><Link to={`/books/${readingSession.book_id}/reading-sessions/${readingSession.id}`}>{readingSession.title}</Link></li>)
   }
 
   return(
     <div className="ReadingSessionList">
-      <h1>Reading Sessions</h1>
-      {renderLinks()}<br/><br/>
+      <h3>Reading Sessions</h3>
       {renderReadingSessions()}
     </div>
   )
