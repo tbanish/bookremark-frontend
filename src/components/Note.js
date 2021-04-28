629ac9136f2d9a8eb6ebec3b3b141a9077415b23
tbanish <tombanish@gmail.com>
2021-04-28 15:23:33 -0400

    add ids notecol1 notecol2 notecol3; render NoteList

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link, Redirect } from 'react-router-dom'
+import NoteList from './NoteList'
 import '../css/Note.css'
 
 const Note = (props) => {
+
   const renderLinks = () => {
     return <Link to={`/books/${props.note && props.note.bookId}/notes/${props.note && props.note.id}/edit`}>edit</Link>
   }
@@ -16,12 +18,27 @@ const Note = (props) => {
     if (props.note) {
       return (
         <>
-          <h1>{props.note && props.note.title}</h1>
+        <div id="notecol1">
+          <NoteList book={props.book} notes={props.notes}/>
+        </div>
+        <div id="notecol2">
+          <h1>{props.book && props.book.attributes.title}</h1>
+          <h2>{props.note && props.note.title}</h2>
           <p>{props.note && props.note.content}</p><br/>
           <div className="noteDeleteEdit">
             <input onClick={() => handleClick()} type="button" value="delete"/>
             {renderLinks()}
           </div>
+        </div>
+        <div id="notecol3">
+          <div>
+            <h3>Links</h3>
+            <Link to={`/books/${props.book && props.book.id}/reading-sessions/new`}>New Reading Session</Link><br/><br/>
+            <Link to={`/books/${props.book && props.book.id}/reading-sessions`}>Reading Sessions</Link><br/><br/>
+            <Link to={`/books/${props.book && props.book.id}/edit`}>Edit Book</Link><br/><br/>
+            <Link to={`/books/${props.book && props.book.id}`}>Book Page</Link>
+          </div>
+        </div>
         </>
       )
     } else {
